ca563c6be3267308f43eac1eeecc581aff48fbd0
xuzhu2017 <a753090302@163.com>
2021-01-22 16:59:14 +0800

    vue配置

--- landM-vue/.history/src/router/index_20210122164435.js
+++ landM-vue/.history/src/router/index_20210122164435.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import AppIndex from '@/components/home/AppIndex'
-import Login from '@/components/Login'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
